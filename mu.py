97dd3a973778b3b42c1f0412c382d200bd95892d
tcramm0nd <25987791+tcramm0nd@users.noreply.github.com>
2020-05-10 23:02:52 -0400

    Edited rules

--- mu.py
+++ mu.py
@@ -3,22 +3,25 @@ class MU:
     def __init__(self):
         self.axiom = 'mi'
         self.theorem = self.axiom
-        self.history = ['mi']
-
+        self.options = [self.ruleI, self.ruleII, self.ruleIII, self.ruleIV]
+        self.state = [self.theorem, self.options]
+        self.history = [self.state]
 
     def __str__(self):
-        print(self.theorem.upper())
+        return self.theorem.upper()
+    def __repr__(self):
+        return self.theorem.upper()
 
     def __getitem__(self, item):
         return [self.history[item]]
 
-    def options(self):
+    def possible(self):
         '''Displays which rules can be applied to the current string
         '''
         if self.theorem[-1] != 'i':
             self.ruleI = False
 
-        if len(self.theorem < 1):
+        if len(self.theorem) < 1:
             self.ruleII = False
 
         if 'iii' not in self.theorem:
@@ -40,31 +43,37 @@ class MU:
         MI -> MIU
 
         '''
-        assert self.theorem.endswith('i'), f"The String {self.theorem} does not \
-                                                end with 'I'"
-
-        self.theorem += 'u'
-        return self.theorem
-
+        if self.options[0]:
+            self.theorem += 'u'
+        else:
+            print('No can do!')
+        self.possible()
     def ruleII(self):
-
-        self.theorem += self.theorem[1:]
-        return self.theorem
+        if self.options[1]:
+            self.theorem += self.theorem[1:]
+        else:
+            print('No can do!')
+        self.possible()
 
     def ruleIII(self, idx):
-
-        self.theorem = self.theorem[:idx] + 'u' + self.theorem[idx+3:]
-        return self.theorem
+        if self.options[2]:
+            self.theorem = self.theorem[:idx] + 'u' + self.theorem[idx+3:]
+        else:
+            print('No can do!')
+        self.possible()
 
     def ruleIV(sel, idx):
-
-        self.theorem = self.theorem[:idx] + self.theorem[idx+2:]
-        return self.theorem
+        if self.options[3]:
+            self.theorem = self.theorem[:idx] + self.theorem[idx+2:]
+        else:
+            print('No can do!')
+        self.possible()
 
 class History(MU):
     '''Displays the history of the string and the rules used to get to that
     state'''
-
+    def __init__(self):
+        self.h = MU.axiom
     def load(self):
         '''Loads a certain historical state'''
     def undo(self):
